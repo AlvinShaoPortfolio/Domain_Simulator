9105816b6e59324aa86615e60176898554d78351
Alvin <alvinshao518@gmail.com>
2026-06-25 03:22:45 -0400

    feat/ added the torus math +display

--- src/simulation.js
+++ src/simulation.js
@@ -1,7 +1,4 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
-const loader = new GLTFLoader();
 
 //creating scene and camera --> renderer takes what the camera sees of the scene and displays iton a canvas.
 const scene = new THREE.Scene();
@@ -13,16 +10,32 @@ document.body.appendChild(renderer.domElement);
 
 scene.background = new THREE.Color(0x222222);
 
-camera.position.set(0, 5, 10);
+camera.position.set(0, 0, 15);
 
 const GESTURE_THRESHOLD = 20;
 let gestureBuffer = null;
 let gestureCount = 0;
 let currentGesture = null;
+let isLoading = false;
+let particleSystem = null;
+let startPositions = null;
+let targetPositions = null;
+let progress = 0;
 
 function animate() {
   requestAnimationFrame(animate);
-  scene.rotation.y += 0.005;
+  scene.rotation.y += 0.020;
+
+  //lerping logic
+  if (particleSystem && progress < 1) {
+    progress += 0.025;
+    const positions = particleSystem.geometry.attributes.position.array;
+    for (let i = 0; i < positions.length; i++) {
+      positions[i] = startPositions[i] + (targetPositions[i] - startPositions[i]) * progress;
+    }
+    particleSystem.geometry.attributes.position.needsUpdate = true;
+  }
+
   renderer.render(scene, camera);
 }
 
@@ -56,20 +69,46 @@ export function updateScene(gesture) {
 }
 
 function initMahoraga() {
-    console.log("mahoraga")
+  console.log("mahoraga")
 
-    loader.load('/mahoraga.glb', (gltf) => {
-      const model = gltf.scene;
-      model.scale.set(0.5, 0.5, 0.5);
-      scene.add(model);
-      console.log('model loaded');
-    });
+  const count = 5000;
+  const radius = 5;
+
+  startPositions = new Float32Array(count*3);
+  targetPositions = new Float32Array(count*3);
+
+  for (let i =0; i < count; i++){
+    startPositions[i*3] = (Math.random() - 0.5) * 30;
+    startPositions[i*3+1] = (Math.random() - 0.5) * 30;
+    startPositions[i*3+2] = (Math.random() - 0.5) * 30;
+
+    const angle = (i / count) * 2 * Math.PI
+    const adjustedRadius = radius + (Math.random() - 0.5) * 2;
+    targetPositions[i * 3] = Math.cos(angle) * adjustedRadius;
+    targetPositions[i * 3 + 1] = Math.sin(angle) * adjustedRadius;
+    targetPositions[i * 3 + 2] = (Math.random() - 0.5) * 2;
+  }
+
+  const positions = new Float32Array(count * 3);
+
+  for (let i = 0; i < positions.length; i++) {
+    positions[i] = startPositions[i];
+  }
+
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+
+  const material = new THREE.PointsMaterial({ color: 0xffffff, size: 0.05 });
+  particleSystem = new THREE.Points(geometry, material);
+  progress = 0;
+  scene.add(particleSystem);
 }
+
 function initCursedSpeech() {
-    console.log("cursedSpeech")
+  console.log("cursedSpeech")
 
 
 }
 function initRabbitEscape() {
-    console.log("rabbitEscape")
+  console.log("rabbitEscape")
 }
